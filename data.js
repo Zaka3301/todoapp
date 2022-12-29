6c59b1db42cf4d6e8acb4dac7ec9f80aff8ab223
Zaka3301 <231466998@formanite.fccollege.edu.pk>
2022-12-30 01:15:04 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 export default [
      {
-        id  : Math.random(), 
+        // id  : Math.random(), 
         title : "first one",
         details: "this is the detail for the first one",
         isDone: true
 
      },
      {
-        id  : Math.random(), 
+        // id  : Math.random(), 
         title: "second one",
         details: "this is the detail for the second one",
         isDone: false 
      },
      {
-        id  : Math.random(), 
+        // id  : Math.random(), 
         title: "third with no details",
         isDone: false
      }
